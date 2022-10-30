96f03e0edebf9de60b573285c2795b07eb8d462a
Annabelle <51344723+annabelledelacruz1825@users.noreply.github.com>
2022-10-30 15:21:10 -0400

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -2,14 +2,14 @@
 # the flat rate is 20%
 flat_tax_rate = .20
 # Standard decduction is $10,00
-standard_deduction= 10,000 
+standard_deduction= 10000 
 # Dependant deduction based per person
 dependent_deduction= 3000 
 
 
 # The user inputs  gross income 
 
-gross_input = int(input(" Enter the gross input "))
+gross_input = float(input(" Enter the gross input "))
 
 # The user input number of dependendents
 
@@ -17,9 +17,9 @@ num_dependants = int(input("Enter the number of dependants "))
 
 # Calculations
 sum_dependent = (dependent_deduction * num_dependants)
-#sum_deduction = (sum_dependent + standard_deduction)
+sum_deduction = sum_dependent + standard_deduction
 
-tax_cal= (gross_input - sum_dependent * (flat_tax_rate)
+tax_cal= (gross_input - sum_deduction ) * (flat_tax_rate)
 #output
 print (f'Gross input: {gross_input}')
 print (f'Number of dependant: {num_dependants}')
